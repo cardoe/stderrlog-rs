32f1d04fc68880bc34149d3996860e624fe7a36a
Zachary Dremann <dremann@gmail.com>
2017-09-05 18:40:52 -0400

    Keep only the highest level modules in the list of modules
    If both `a::b` and `a::b::c` are inserted, keep only `a::b`. This
    ensures that the first item before a given submodule `a::b::d` will be
    either a supermodule, or a different module completely

--- src/lib.rs
+++ src/lib.rs
@@ -288,7 +288,14 @@ impl StdErrLog {
         let to_insert = module.into();
         // If Ok, the module was already found
         if let Err(i) = self.modules.binary_search(&to_insert) {
-            self.modules.insert(i, to_insert);
+            // If a super-module of the current module already exists, don't insert this module
+            if i == 0 || !is_submodule(&self.modules[i - 1], &to_insert) {
+                // Remove any submodules of the module we're inserting
+                if let Some(submodule_count) = self.modules[i..].iter().position(|possible_submodule| !is_submodule(&to_insert, possible_submodule)) {
+                    self.modules.drain(i..i+submodule_count);
+                }
+                self.modules.insert(i, to_insert);
+            }
         }
         self
     }
