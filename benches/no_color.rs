1136c148ae6bb1942cf797eca6597c5a0d50a065
Jonathan Creekmore <jonathan.creekmore@starlab.io>
2017-09-05 13:15:53 -0500

    colorize the log-level output

---
+++ benches/no_color.rs
@@ -0,0 +1,26 @@
+#![feature(test)]
+extern crate test;
+extern crate stderrlog;
+extern crate libc;
+
+#[macro_use]
+extern crate log;
+extern crate termcolor;
+
+mod util;
+mod common;
+
+use std::sync;
+
+static INIT_LOGGER: sync::Once = sync::ONCE_INIT;
+
+fn init_logger() {
+    INIT_LOGGER.call_once(|| {
+        stderrlog::StdErrLog::new()
+            .timestamp(stderrlog::Timestamp::Second)
+            .verbosity(10)
+            .color(termcolor::ColorChoice::Never)
+            .module(module_path!())
+            .init().unwrap();
+    });
+}
