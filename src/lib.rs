bff23d5c91ddc63459e12bd6299162f7cf317050
Doug Goldstein <cardoe@cardoe.com>
2017-09-05 09:17:13 -0500

    extend docs examples with clap and timestamp
    Add a clap example to the docs and include info about the timestamp to
    the docs.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 //! verbosity the higher the log level. Additionally it supports the
 //! ability to provide timestamps at different granularities.
 //!
-//! ### Examples
+//! ## Simple Use Case
 //!
 //! ```rust
 //! #[macro_use]
@@ -26,6 +26,8 @@
 //!     // ...
 //! }
 //! ```
+//!
+//! ## docopt Example
 //!
 //! ```rust
 //! extern crate docopt;
@@ -66,6 +68,64 @@
 //!
 //!     // ...
 //! }
+//! ```
+//!
+//! # clap Example
+//!
+//! ```
+//! #[macro_use]
+//! extern crate clap;
+//! #[macro_use]
+//! extern crate log;
+//! extern crate stderrlog;
+//!
+//! use clap::{Arg, App};
+//!
+//! fn main() {
+//!     let m = App::new("stderrlog example")
+//!         .version(crate_version!())
+//!         .arg(Arg::with_name("verbosity")
+//!              .short("v")
+//!              .multiple(true)
+//!              .help("Increase message verbosity"))
+//!         .arg(Arg::with_name("quiet")
+//!              .short("q")
+//!              .help("Silence all output"))
+//!         .arg(Arg::with_name("timestamp")
+//!              .short("t")
+//!              .help("prepend log lines with a timestamp")
+//!              .takes_value(true)
+//!              .possible_values(&["none", "sec", "ms", "ns"]))
+//!         .get_matches();
+//!
+//!     let verbose = m.occurrences_of("verbosity") as usize;
+//!     let quiet = m.is_present("quiet");
+//!     let ts = match m.value_of("timestamp") {
+//!         Some("ns") => stderrlog::Timestamp::Nanosecond,
+//!         Some("ms") => stderrlog::Timestamp::Microsecond,
+//!         Some("sec") => stderrlog::Timestamp::Second,
+//!         Some("none") | None => stderrlog::Timestamp::Off,
+//!         Some(_) => clap::Error {
+//!             message: "invalid value for 'timestamp'".into(),
+//!             kind: clap::ErrorKind::InvalidValue,
+//!             info: None,
+//!         }.exit(),
+//!     };
+//!
+//!     stderrlog::new()
+//!         .module(module_path!())
+//!         .quiet(quiet)
+//!         .verbosity(verbose)
+//!         .timestamp(ts)
+//!         .init()
+//!         .unwrap();
+//!     trace!("trace message");
+//!     debug!("debug message");
+//!     info!("info message");
+//!     warn!("warn message");
+//!     error!("error message");
+//! }
+//! ```
 
 extern crate chrono;
 extern crate log;
